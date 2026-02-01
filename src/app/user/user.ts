3d46a203f790f22feb3abd8e64b00cfe0acbaec6
Kashyap Solanki <kash.cdac@gmail.com>
2026-02-01 18:18:50 +0530

    signals-integrated

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { DUMMY_USERS, IDummyUser } from '../../data/dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+const randomIndex = () => Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,13 +11,22 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class User {
-  selectedUser: IDummyUser = DUMMY_USERS[randomIndex];
+  // selectedUser: IDummyUser = DUMMY_USERS[randomIndex()];
+  selectedUser = signal(DUMMY_USERS[randomIndex()]);
+  // signal is like a container that contains the value of selectedUser
+  // so when the value of selectedUser is changed Angular is notified that selectedUser is changed so need to make necessary changes, in places where that value needs to change
 
-  get getImagePath() {
-    return this.selectedUser.avatar;
+  get imagePath() {
+    return this.selectedUser().avatar;
+  }
+
+  getSelectedUser(): IDummyUser {
+    return this.selectedUser();
   }
 
   onSelectUser() {
     console.log('Clicked');
+    // this.selectedUser = DUMMY_USERS[randomIndex()];
+    this.selectedUser.set(DUMMY_USERS[randomIndex()]);
   }
 }
